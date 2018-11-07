99caad0ec6084abaac4932a7da77fa32a216826a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-06 17:42:27 -0800

    evil_polygons_&_code4life_IP

--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -10,15 +10,15 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
+		Robot max = new Robot();
 
 		//2. Set the speed to 100
-
+		max.setSpeed(100);
 		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
-		//4. Ask the use how many polygons they want to be drawn.
-		
+		max.penDown();
+		//4. Ask the user how many polygons they want to be drawn.
+		String answer = JOptionPane.showInputDialog("What shape would you like");
 		//5. Use the robot to draw the number of polygons the user requested.
 		
 		//6. Make it so your shapes do not overlap
