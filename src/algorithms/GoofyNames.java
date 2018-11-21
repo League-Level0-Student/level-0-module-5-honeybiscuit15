d2c0fe9ce56b9b35de484d1330ea248366dc28e6
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-20 17:55:34 -0800

    evilpolygon_IP&&goffyName

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -8,20 +8,22 @@ import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	public static void main(String[] args) {
-		String goofyName = "";
+		String goofyName = 
 
 		// 1. Ask the user to enter their name
-
+JOptionPane.showInputDialog(null,"plz enter ur name");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+System.out.println(goofyName.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
+goofyName.length();
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
+String otherName = 
+JOptionPane.showInputDialog(null,"plz enter ur name");
+
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
 
--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -18,19 +18,20 @@ public class EvilPolygonsChallenge {
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
 		max.penDown();
 		//4. Ask the user how many polygons they want to be drawn.
-		String answer = JOptionPane.showInputDialog("What shape would you like");
+		String answer = JOptionPane.showInputDialog("how many polygons you want to be drawn");
 		//5. Use the robot to draw the number of polygons the user requested.
+		
 		static void drawSquare() {
 			
-			for (int j = 0; j < 5.; j++) {
+			for (int j = 0; j < 5.; j++){
 				
 				max.move(200);
 				max.turn(90);
 			}
 		
 		}
-		static void drawTriangle() {
-			for(int i = 0; i<4.; i++) {
+		static void drawTriangle(){
+			for(int i = 0; i<4.; i++){
 				
 				max.move(200);
 				max.turn(120);
@@ -38,7 +39,7 @@ public class EvilPolygonsChallenge {
 			
 			}
 		}
-		static void drawCircle() {
+		static void drawCircle(){
 			max.turn(1);
 	} 
 		//6. Make it so your shapes do not overlap
