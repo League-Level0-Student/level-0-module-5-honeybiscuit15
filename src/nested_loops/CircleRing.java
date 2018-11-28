725c0228f8d0e4efd2e96c23763ed854bd130cc5
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-27 17:59:00 -0800

    11-26-18

--- src/nested_loops/CircleRing.java
+++ src/nested_loops/CircleRing.java
@@ -10,17 +10,17 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
-
-
+		Robot max = new Robot();
        //  2. Set your robot’s position to x=150 and y=200
-
+max.setX(150);
+max.setY(200);
         // 3. Put the robot's pen down
-
+max.penDown();
         // 4. Set the robot’s speed to 10
-
+max.setSpeed(10);
         // 5. Do everything below here 360 times (use i as the counter)
 
-	        // 6. Move the robot 3 pixels
+// 6. Move the robot 3 pixels
 
        //  7. Turn the robot 1 degree
 
